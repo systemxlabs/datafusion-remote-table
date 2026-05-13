f97681036d54206d51b8d3e2c996213c20c7db5c
张林伟 <lewiszlw520@gmail.com>
2026-05-13 17:19:24 +0800

    Fix mysql integration tests for datafusion 53

--- integration-tests/tests/mysql.rs
+++ integration-tests/tests/mysql.rs
@@ -87,11 +87,12 @@ async fn pushdown_limit(#[case] source: RemoteSource) {
         RemoteDbType::Mysql,
         "describe simple_table",
         "SELECT * FROM remote_table limit 1",
-        r#"+-------+------+------+-----+---------+-------+
-| Field | Type | Null | Key | Default | Extra |
-+-------+------+------+-----+---------+-------+
-| id    | int  | NO   | PRI |         |       |
-+-------+------+------+-----+---------+-------+"#,
+        r#"+-------+--------------+------+-----+---------+-------+
+| Field | Type         | Null | Key | Default | Extra |
++-------+--------------+------+-----+---------+-------+
+| id    | int          | NO   | PRI |         |       |
+| name  | varchar(255) | NO   |     |         |       |
++-------+--------------+------+-----+---------+-------+"#,
     )
     .await;
 }
