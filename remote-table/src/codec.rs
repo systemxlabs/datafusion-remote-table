0ee467951fd503b8e5e30eed1c8f84cb0872fefa
Linwei Zhang <lewiszlw520@gmail.com>
2026-07-24 18:12:11 +0800

    refactor: use ExecutionPlan::downcast_ref() instead of raw Any downcast
    DataFusion 54 provides `downcast_ref::<T>()` and `is::<T>()` as inherent
    methods on `dyn ExecutionPlan`, available on `Arc<dyn ExecutionPlan>` via
    auto-deref. This is cleaner than `(&*node as &dyn Any).downcast_ref()` and
    correctly handles `downcast_delegate()` for wrapper plans.

--- remote-table/src/codec.rs
+++ remote-table/src/codec.rs
@@ -22,7 +22,6 @@ use datafusion_proto::physical_plan::PhysicalExtensionCodec;
 use datafusion_proto::protobuf::proto_error;
 use derive_with::With;
 use prost::Message;
-use std::any::Any;
 use std::fmt::Debug;
 use std::sync::Arc;
 use std::time::Duration;
@@ -207,7 +206,7 @@ impl PhysicalExtensionCodec for RemotePhysicalCodec {
     }
 
     fn try_encode(&self, node: Arc<dyn ExecutionPlan>, buf: &mut Vec<u8>) -> DFResult<()> {
-        if let Some(exec) = (&*node as &dyn Any).downcast_ref::<RemoteTableScanExec>() {
+        if let Some(exec) = node.downcast_ref::<RemoteTableScanExec>() {
             let serialized_transform = if exec.transform.is::<DefaultTransform>() {
                 DefaultTransformCodec {}.try_encode(exec.transform.as_ref())?
             } else {
@@ -244,7 +243,7 @@ impl PhysicalExtensionCodec for RemotePhysicalCodec {
                 ))
             })?;
             Ok(())
-        } else if let Some(exec) = (&*node as &dyn Any).downcast_ref::<RemoteTableInsertExec>() {
+        } else if let Some(exec) = node.downcast_ref::<RemoteTableInsertExec>() {
             let serialized_connection_options = serialize_connection_options(&exec.conn_options);
             let remote_schema = serialize_remote_schema(&exec.remote_schema);
             let serialized_table = protobuf::Identifiers {
